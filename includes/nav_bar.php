5b17bfb939a271f3f1aad8517403e8d2bfb67df3
Cj-fe <us1071591@gmail.com>
2025-02-28 09:28:46 +0800

    commit

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -1,47 +1,47 @@
- <!-- Top Bar with logo, contact info and consultation button -->
- <div class="top-bar">
-        <div class="logo-container">
-            <img src="img/481741660_1202479434928856_876828406647000477_n.png" alt="Eco Energy Logo" class="logo-image">
-        </div>
-
-        <div class="contact-info">
-            <div class="contact-item">
-                <i class="contact-icon fa-solid fa-phone"></i>
-                <div class="contact-text">
-                    <span class="contact-heading">Call Us Anytime</span>
-                    <span class="contact-detail">(+91) 1800-214-122</span>
-                </div>
-            </div>
-
-            <div class="contact-item">
-                <i class="contact-icon fa-solid fa-envelope"></i>
-                <div class="contact-text">
-                    <span class="contact-heading">Email Us Anytime</span>
-                    <span class="contact-detail">eco@example.com</span>
-                </div>
-            </div>
-        </div>
-
+<!-- Top Bar with logo, contact info and consultation button -->
+<div class="top-bar">
+    <div class="logo-container">
+        <img src="img/480521352_1748505962663217_598372962596217603_n.png" alt="Eco Energy Logo" class="logo-image">
     </div>
 
-    <!-- Main Navigation Bar -->
-    <div class="navigation">
-        <ul class="nav-links">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="/about.html">About Us</a></li>
-            <li><a href="project.php">Projects</a></li>
-            <li><a href="#recycle">Methodology</a></li>
-        </ul>
-
-        <div class="social-search">
-            <div class="search-icon">
-                <i class="fa-solid fa-magnifying-glass"></i>
+    <div class="contact-info">
+        <div class="contact-item">
+            <i class="contact-icon fa-solid fa-phone"></i>
+            <div class="contact-text">
+                <span class="contact-heading">Call Us Anytime</span>
+                <span class="contact-detail">(+91) 1800-214-122</span>
             </div>
-            <div class="social-icons">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+        </div>
+
+        <div class="contact-item">
+            <i class="contact-icon fa-solid fa-envelope"></i>
+            <div class="contact-text">
+                <span class="contact-heading">Email Us Anytime</span>
+                <span class="contact-detail">eco@example.com</span>
             </div>
         </div>
     </div>
+
+</div>
+
+<!-- Main Navigation Bar -->
+<div class="navigation">
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="project.php">Projects</a></li>
+        <li><a href="#recycle">Methodology</a></li>
+    </ul>
+
+    <div class="social-search">
+        <div class="search-icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="social-icons">
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+        </div>
+    </div>
+</div>
