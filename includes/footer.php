fb871ca4b37fb5e7dd5fc64fc0cb51f0fcf8dc6a
louilal123 <rubinlouie41@gmail.com>
2025-02-28 11:22:13 +0800

    asdf

--- includes/footer.php
+++ includes/footer.php
@@ -1,54 +1,54 @@
 <footer class="footer">
-    <div class="footer-container">
-        <!-- About Section -->
-        <div class="footer-column about">
-            <img src="img/480521352_1748505962663217_598372962596217603_n.png" alt="GoodLand Logo" class="footer-logo">
-            <p>An art-driven NGO focused on sustainable solutions for social, economic, and environmental challenges on Bantayan Island.</p>
-            <h4>Follow Us</h4>
-            <div class="footer-social">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-            </div>
-        </div>
-
-        <!-- Quick Links -->
-        <div class="footer-column">
-            <h3>Quick Links</h3>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Methodology</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Get Involved</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-
-        <!-- Recent Posts -->
-        <div class="footer-column">
-            <h3>Recent Posts</h3>
-            <div class="post-item">
-                <img src="post1.jpg" alt="Post Image">
-                <div class="post-info">
-                    <p><strong>Go Green Initiative Takes Off</strong></p>
-                    <span>by Admin</span>
+        <div class="footer-container">
+            <!-- About Section -->
+            <div class="footer-column about">
+                <img src="img/480521352_1748505962663217_598372962596217603_n.png" alt="GoodLand Logo" class="footer-logo">
+               <p>You dont have to follow us into the ocean but you can follow us in social media.</p>
+                <h4>Follow Us</h4>
+                <div class="footer-social">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
                 </div>
             </div>
-            <div class="post-item">
-                <img src="post2.jpg" alt="Post Image">
-                <div class="post-info">
-                    <p><strong>Empowering Coastal Communities</strong></p>
-                    <span>by Admin</span>
+            
+            <!-- Quick Links -->
+            <div class="footer-column">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Methodology</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Get Involved</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            
+            <!-- Recent Posts -->
+            <div class="footer-column">
+                <h3>Recent Posts</h3>
+                <div class="post-item">
+                    <img src="../img/adlaw.jpg.jpg">
+                    <div class="post-info">
+                        <p><strong>Go Green Initiative Takes Off</strong></p>
+                        <span>by Admin</span>
+                    </div>
+                </div>
+                <div class="post-item">
+                <img src="../img/adlaw.jpg.jpg">
+                    <div class="post-info">
+                        <p><strong>Empowering Coastal Communities</strong></p>
+                        <span>by Admin</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- Bottom Footer -->
-    <div class="footer-bottom">
-        <hr>
-        <p>© 2025 GoodLand. All Rights Reserved.</p>
-    </div>
-</footer>
+        
+        <!-- Bottom Footer -->
+        <div class="footer-bottom">
+            <hr>
+            <p>© 2025 GoodLand. All Rights Reserved.</p>
+        </div>
+    </footer>
