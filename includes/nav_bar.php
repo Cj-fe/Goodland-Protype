fb08779341533eaa0052ef88265f5fb60d91bd08
Cj-fe <us1071591@gmail.com>
2025-02-28 09:31:56 +0800

    gi

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -41,7 +41,6 @@
             <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
             <a href="#"><i class="fa-brands fa-twitter"></i></a>
             <a href="#"><i class="fa-brands fa-instagram"></i></a>
-            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
         </div>
     </div>
 </div>
