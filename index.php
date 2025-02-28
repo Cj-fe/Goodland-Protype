aaa109f825760ffc9aea899af3e8d18c8ec146f1
Cj-fe <us1071591@gmail.com>
2025-02-28 09:02:07 +0800

    commit

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="footer.css">
 </head>
 
 <body>
@@ -473,6 +474,10 @@
             </div>
         </div>
     </section>
+
+    <!-- Footer Section -->
+    <?php include 'includes/footer.php'?>
+
 
     <script src="js/index_script.js"></script>
 
